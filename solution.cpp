90418c41f6b5a0c8def00003e8ae621aeee16ea9
thibaut54 <thibaut54@users.noreply.github.com>
2026-04-26 12:09:36 +0200

    A+B solution

---
+++ solution.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    long long a, b;
+    std::cin >> a >> b;
+    std::cout << a + b << '\n';
+    return 0;
+}
